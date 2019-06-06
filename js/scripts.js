c94a8b9c63b8b638022f7793f90f5e4b7ff9d12b
Joseph Tubridy and Trevor Teigen <trevor@>
2019-06-06 10:52:14 -0700

    added CSS alternative with green highlight

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,34 @@ $(function(){
   var height;
   $("#userInput").submit(function(event){
   height = parseInt($("input#height").val());
+
   if (height < 42 ) {
-  $('#middle').hide();
-  $('#tall').hide();
-  $('#short').show();
+  // $('#middle').hide();
+  // $('#tall').hide();
+  // $('#short').show();
+
+  $(".myRides").show();
+  $(".tallName").removeClass("rideable");
+  $(".middleName").removeClass("rideable");
+  $(".shortName").addClass("rideable");
 } else if (height >= 54) {
-  $('#middle').show();
-  $('#tall').show();
-  $('#short').hide();
+  // $('#middle').show();
+  // $('#tall').show();
+  // $('#short').hide();
+
+  $(".myRides").show();
+  $(".shortName").removeClass("rideable");
+  $(".middleName").addClass("rideable");
+  $(".tallName").addClass("rideable");
 } else {
-  $('#short').show();
-  $('#middle').show();
-  $('#tall').show();
+  // $('#short').show();
+  // $('#middle').show();
+  // $('#tall').show();
+
+  $(".myRides").show();
+  $(".shortName").addClass("rideable");
+  $(".middleName").addClass("rideable");
+  $(".tallName").removeClass("rideable");
 }
 event.preventDefault();
 })
